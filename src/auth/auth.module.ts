a3d54247cf132ccc6d0082e59b7b5f0e1327cada
Kayle54187 <habichristi@gmail.com>
2023-06-09 21:36:50 +0200

    Adding Auth Controller and Auth Service

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,5 +1,8 @@
 import { Module } from "@nestjs/common/decorators";
-
-
-@Module({})
+import { AuthController } from "./auth.controller";
+import { AuthService } from "./auth.service";
+@Module({
+    controllers: [AuthController],
+    providers: [AuthService],
+})
 export class AuthModule {}
